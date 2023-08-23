66d8d9e6138d6c52acdc6430678f535666374418
tshegofatso <tshego.tt.morare@gmail.com>
2023-08-23 16:25:44 -0700

    pall.c

---
+++ pall.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * pall - Prints all elements in the stack.
+ * @stack: Pointer to the stack.
+ * @line_number : args for line number
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+
+	stack_t *current = *stack;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
